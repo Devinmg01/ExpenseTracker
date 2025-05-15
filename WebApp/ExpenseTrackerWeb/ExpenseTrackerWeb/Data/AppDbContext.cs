5ad2d57008985b99c09cc322b300a474429929e5
Devinmg01 <79491891+Devinmg01@users.noreply.github.com>
2025-05-15 10:50:38 -0700

    structure made

---
+++ WebApp/ExpenseTrackerWeb/ExpenseTrackerWeb/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ExpenseTrackerWeb.Models;
+
+namespace ExpenseTrackerWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
---
+++ WebApp/ExpenseTrackerWeb/ExpenseTrackerWeb/Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTrackerWeb.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public float Amount { get; set; }
+
+        [Required]
+        public string Type { get; set; } 
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public string? Notes { get; set; }
+    }
+}
